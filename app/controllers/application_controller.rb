646fdf97cb1d1b4d82dca711bef0e531da8944fd
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-09-10 20:57:41 +0000

    Some authorization and other smaller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   skip_forgery_protection
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back fallback_location: root_url
+  end
 end
--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -76,21 +76,21 @@ class JobApplicationsController < ApplicationController
     render({ :template => "job_application_templates/show" })
   end
 
-  def create
-    the_job_application = JobApplication.new
-    the_job_application.user_id = params.fetch("query_user_id")
-    the_job_application.application_date = params.fetch("query_application_date")
-    the_job_application.status = params.fetch("query_status")
-    the_job_application.description_id = params.fetch("query_description_id")
-    the_job_application.interview_date = params.fetch("query_interview_date")
+  # def create
+  #   the_job_application = JobApplication.new
+  #   the_job_application.user_id = params.fetch("query_user_id")
+  #   the_job_application.application_date = params.fetch("query_application_date")
+  #   the_job_application.status = params.fetch("query_status")
+  #   the_job_application.description_id = params.fetch("query_description_id")
+  #   the_job_application.interview_date = params.fetch("query_interview_date")
 
-    if the_job_application.valid?
-      the_job_application.save
-      redirect_to("/job_applications", { :notice => "Job application created successfully." })
-    else
-      redirect_to("/job_applications", { :alert => the_job_application.errors.full_messages.to_sentence })
-    end
-  end
+  #   if the_job_application.valid?
+  #     the_job_application.save
+  #     redirect_to("/job_applications", { :notice => "Job application created successfully." })
+  #   else
+  #     redirect_to("/job_applications", { :alert => the_job_application.errors.full_messages.to_sentence })
+  #   end
+  # end
 
   def update
     the_id = params.fetch("path_id")
