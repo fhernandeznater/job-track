fcf47f67d73107e6b4db0147e4595b60f5eda1a8
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-09-12 20:36:16 +0000

    Overhaul of the show page

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -97,20 +97,52 @@ class JobApplicationsController < ApplicationController
 
   def update
     the_id = params.fetch("path_id")
-    the_job_application = JobApplication.where({ :id => the_id }).at(0)
+    @the_job_application = JobApplication.where({ :id => the_id }).at(0)
 
-    the_job_application.user_id = params.fetch("query_user_id")
-    the_job_application.application_date = params.fetch("query_application_date")
-    the_job_application.status = params.fetch("query_status")
-    the_job_application.description_id = params.fetch("query_description_id")
-    the_job_application.interview_date = params.fetch("query_interview_date")
+    form_data = params.require("job_application")
 
-    if the_job_application.valid?
-      the_job_application.save
-      redirect_to("/job_applications/#{the_job_application.id}", { :notice => "Job application updated successfully." } )
-    else
-      redirect_to("/job_applications/#{the_job_application.id}", { :alert => the_job_application.errors.full_messages.to_sentence })
+    company_name = form_data.fetch("company_name")
+    company = Company.where({ :name => company_name }).at(0)
+
+    unless company
+      company = Company.new
+      company.name = company_name
+      company.save
     end
+
+    job_title = form_data.fetch("job_title")
+    category = form_data.fetch("category")
+    start_date = form_data.fetch("start_date")
+    description = JobDescription.where({
+      :company_id => company.id,
+      :job_title => job_title,
+      :category => category,
+      :start_date => start_date
+    }).at(0)
+
+    unless description
+      description = JobDescription.new
+      description.company_id = company.id
+      description.job_title = job_title
+      description.category = category
+      description.start_date = start_date
+      description.save
+    end
+
+    @the_job_application.description_id = description.id
+    @the_job_application.application_date = form_data.fetch("application_date")
+    @the_job_application.status = form_data.fetch("status")
+    @the_job_application.interview_date = form_data.fetch("interview_date", nil)
+
+    authorize(@the_job_application)
+
+    if @the_job_application.save
+      redirect_to("/job_applications/#{@the_job_application.id}", { :notice => "Job application updated successfully." } )
+    else
+      redirect_to("/job_applications/#{@the_job_application.id}", { :alert => @the_job_application.errors.full_messages.to_sentence })
+    end
+
+    return
   end
 
   def destroy
